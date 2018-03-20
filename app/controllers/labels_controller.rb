15771855493a8f933f98f1db56b8b84027a4100d
Ian Remillard <Doom-Mac@Ians-iMac.local>
2018-03-19 17:33:35 -0700

    can save labels

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -8,5 +8,12 @@ class LabelsController < ApplicationController
   end
 
   def create
+    @label = Label.new(params[:label].permit(:height, :length, :width, :weight))
+    from_address = Address.new(params[:label][:from_address].permit(:name, :company, :street1, :street2, :city, :state, :zip, :phone))
+    to_address = Address.new(params[:label][:to_address].permit(:name, :company, :street1, :street2, :city, :state, :zip, :phone))
+    from_address.is_to_address = false
+    to_address.is_to_address = true
+    @label.addresses = [from_address, to_address]
+    @label.save!
   end
 end
